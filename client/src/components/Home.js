2f587570760a671d1852e31034ca7e899a819c87
Harsh Kumar <hk07hrs2004@gmail.com>
2025-06-11 21:46:58 +0530

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,70 +3,53 @@ import Header from './Header'
 import Todo from './Todo'
 import Addmodel from './Addmodel'
 import axios from 'axios'
+
 function Home() {
   const [allTodos, setAllTodos] = useState([])
   const [refresh ,setRefresh]= useState()
+
   function getTokens(){
     const user =  localStorage.getItem('user')
     if(!user){
-        return
+        return null;
     }
     const token = JSON.parse(user).token
     return token
-
   }
 
-  
   useEffect(() => {  
     const token = getTokens()
     if(!token){
       window.location.href = '/'
+    } else {
+      getTodos(token)
     }
-    getTodos()
-
   },[refresh])
-   
-  
-  async function getTodos() {
+
+  async function getTodos(token) {
     try {
-      // Assuming getTokens() returns a Promise or the token itself
-      const token = await getTokens();
-  
       const response = await axios.get('http://localhost:5000/api/todo', {
         headers: {
           auth: token,
         },
       });
-       let a=response.data.user.todos
       setAllTodos(response.data.user.todos);
-      // console.log(a)
     } catch (error) {
       console.error(error);
     }
   }
 
-
-
-  
-
-
   return (
     <>
-    <Header />
-    <div className="container" style={{position:"relative"}}>
-      <div className="row justify-content-md-center mt-4">
-        {allTodos.map((todo)=>{
-          return <Todo key={todo._id} id={todo._id} todo={todo.desc} isdone={todo.isDone} setRefresh={setRefresh}/>
-        })}
-        {/* <Todo/>
-        <Todo/>
-        <Todo/>
-        <Todo/> */}
+      <Header />
+      <div className="container" style={{position:"relative"}}>
+        <div className="row justify-content-md-center mt-4">
+          {allTodos.map((todo)=>(
+            <Todo key={todo._id} id={todo._id} todo={todo.desc} isdone={todo.isDone} setRefresh={setRefresh}/>
+          ))}
+        </div>
+        <Addmodel setRefresh={setRefresh }/>
       </div>
-      <Addmodel setRefresh={setRefresh }/>
-
-    </div>
-
     </>
   )
 }
